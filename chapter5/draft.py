76c828c5f96ef5fff65abfbeb7f004df5d7aa2cb
Luiza Pagliari <lpagliar@thoughtworks.com>
2013-10-23 06:50:38 -0500

    Files for chapter5

---
+++ chapter5/draft.py
@@ -0,0 +1,37 @@
+import optimization
+s = [1,4,3,2,7,3,6,3,2,4,5,3]
+
+reload(optimization)
+domain = [(0,9)] * (len(optimization.people) * 2)
+s = optimization.randomoptimize(domain, optimization.schedulecost)
+optimization.schedulecost(s)
+optimization.printschedule(s)
+
+s = optimization.hillclimb(domain, optimization.schedulecost)
+optimization.schedulecost(s)
+optimization.printschedule(s)
+
+s = optimization.annealingoptimize(domain, optimization.schedulecost, T=10000, cool=0.95, step=3)
+optimization.schedulecost(s)
+optimization.printschedule(s)
+
+s = optimization.geneticoptimize(domain, optimization.schedulecost)
+optimization.printschedule(s)
+
+#######################
+import dorm
+dorm.printsolution([0,0,0,0,0,0,0,0,0,0])
+
+s = optimization.randomoptimize(dorm.domain, dorm.dormcost)
+dorm.dormcost(s)
+s = optimization.geneticoptimize(dorm.domain, dorm.dormcost)
+dorm.printsolution(s)
+
+#######################
+import socialnetwork
+import optimization
+sol = optimization.randomoptimize(socialnetwork.domain, socialnetwork.crosscount)
+socialnetwork.crosscount(sol)
+sol = optimization.annealingoptimize(socialnetwork.domain, socialnetwork.crosscount, step = 50, cool = 0.99)
+socialnetwork.crosscount( sol)
+

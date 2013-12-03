39c71656570b12ca97075f241fbcd400e532b563
Luiza Pagliari <lpagliar@thoughtworks.com>
2013-12-03 05:49:48 -0600

    Other changes for chapter 9

--- chapter9/advancedclassify.py
+++ chapter9/advancedclassify.py
@@ -61,19 +61,22 @@ def dpclassify(point, avgs):
   if y > 0: return 0
   else:     return 1
 
-#def yesno(v):
-#  if v=='yes': return 1
-#  elif v=='no': return -1
-#  else: return 0
-#
-#def matchcount(interest1,interest2):
-#  l1=interest1.split(':')
-#  l2=interest2.split(':')
-#  x=0
-#  for v in l1:
-#    if v in l2: x+=1
-#  return x
-#
+def yesno(v):
+  if v=='yes': return 1
+  elif v=='no': return -1
+  else: return 0
+
+def matchcount(interest1,interest2):
+  l1=interest1.split(':')
+  l2=interest2.split(':')
+  x=0
+  for v in l1:
+    if v in l2: x+=1
+  return x
+
+def milesdistance(a1,a2):
+  return 0
+
 #yahookey="YOUR API KEY"
 #from xml.dom.minidom import parseString
 #from urllib import urlopen,quote_plus
@@ -97,73 +100,73 @@ def dpclassify(point, avgs):
 #  longdif=53.0*(long2-long1)
 #  return (latdif**2+longdif**2)**.5
 #
-#def loadnumerical():
-#  oldrows=loadmatch('matchmaker.csv')
-#  newrows=[]
-#  for row in oldrows:
-#    d=row.data
-#    data=[float(d[0]),yesno(d[1]),yesno(d[2]),
-#          float(d[5]),yesno(d[6]),yesno(d[7]),
-#          matchcount(d[3],d[8]),
-#          milesdistance(d[4],d[9]),
-#          row.match]
-#    newrows.append(matchrow(data))
-#  return newrows
-#
-#def scaledata(rows):
-#  low=[999999999.0]*len(rows[0].data)
-#  high=[-999999999.0]*len(rows[0].data)
-#  # Find the lowest and highest values
-#  for row in rows:
-#    d=row.data
-#    for i in range(len(d)):
-#      if d[i]<low[i]: low[i]=d[i]
-#      if d[i]>high[i]: high[i]=d[i]
-#
-#  # Create a function that scales data
-#  def scaleinput(d):
-#     return [(d[i]-low[i])/(high[i]-low[i])
-#            for i in range(len(low))]
-#
-#  # Scale all the data
-#  newrows=[matchrow(scaleinput(row.data)+[row.match])
-#           for row in rows]
-#
-#  # Return the new data and the function
-#  return newrows,scaleinput
-#
-#
-#def rbf(v1,v2,gamma=10):
-#  dv=[v1[i]-v2[i] for i in range(len(v1))]
-#  l=veclength(dv)
-#  return math.e**(-gamma*l)
-#
-#def nlclassify(point,rows,offset,gamma=10):
-#  sum0=0.0
-#  sum1=0.0
-#  count0=0
-#  count1=0
-#
-#  for row in rows:
-#    if row.match==0:
-#      sum0+=rbf(point,row.data,gamma)
-#      count0+=1
-#    else:
-#      sum1+=rbf(point,row.data,gamma)
-#      count1+=1
-#  y=(1.0/count0)*sum0-(1.0/count1)*sum1+offset
-#
-#  if y>0: return 0
-#  else: return 1
-#
-#def getoffset(rows,gamma=10):
-#  l0=[]
-#  l1=[]
-#  for row in rows:
-#    if row.match==0: l0.append(row.data)
-#    else: l1.append(row.data)
-#  sum0=sum(sum([rbf(v1,v2,gamma) for v1 in l0]) for v2 in l0)
-#  sum1=sum(sum([rbf(v1,v2,gamma) for v1 in l1]) for v2 in l1)
-#
-#  return (1.0/(len(l1)**2))*sum1-(1.0/(len(l0)**2))*sum0
-#
+def loadnumerical():
+  oldrows=loadmatch('matchmaker.csv')
+  newrows=[]
+  for row in oldrows:
+    d=row.data
+    data=[float(d[0]),yesno(d[1]),yesno(d[2]),
+          float(d[5]),yesno(d[6]),yesno(d[7]),
+          matchcount(d[3],d[8]),
+          milesdistance(d[4],d[9]),
+          row.match]
+    newrows.append(matchrow(data))
+  return newrows
+
+def scaledata(rows):
+  low  =[999999999.0]  * len(rows[0].data)
+  high =[-999999999.0] * len(rows[0].data)
+  # Find the lowest and highest values
+  for row in rows:
+    d = row.data
+    for i in range(len(d)):
+      if d[i] < low[i]:  low[i]  = d[i]
+      if d[i] > high[i]: high[i] = d[i]
+
+  # Create a function that scales data
+  def scaleinput(d):
+     return [(d[i] - low[i]) / (high[i] - low[i])
+            for i in range(len(low))]
+
+  # Scale all the data
+  newrows=[matchrow(scaleinput(row.data) + [row.match])
+           for row in rows]
+
+  # Return the new data and the function
+  return newrows,scaleinput
+
+from math import e
+
+def rbf(v1,v2,gamma=10):
+  dv=[v1[i]-v2[i] for i in range(len(v1))]
+  l=veclength(dv)
+  return e**(-gamma*l)
+
+def nlclassify(point,rows,offset,gamma=10):
+  sum0=0.0
+  sum1=0.0
+  count0=0
+  count1=0
+
+  for row in rows:
+    if row.match==0:
+      sum0+=rbf(point,row.data,gamma)
+      count0+=1
+    else:
+      sum1+=rbf(point,row.data,gamma)
+      count1+=1
+  y=(1.0/count0)*sum0-(1.0/count1)*sum1+offset
+
+  if y>0: return 0
+  else: return 1
+
+def getoffset(rows,gamma=10):
+  l0=[]
+  l1=[]
+  for row in rows:
+    if row.match==0: l0.append(row.data)
+    else: l1.append(row.data)
+  sum0=sum(sum([rbf(v1,v2,gamma) for v1 in l0]) for v2 in l0)
+  sum1=sum(sum([rbf(v1,v2,gamma) for v1 in l1]) for v2 in l1)
+
+  return (1.0/(len(l1)**2))*sum1-(1.0/(len(l0)**2))*sum0
--- chapter9/draft.py
+++ chapter9/draft.py
@@ -9,3 +9,20 @@ reload(advancedclassify)
 advancedclassify.dpclassify([30,30], avgs)
 advancedclassify.dpclassify([48,32], avgs)
 advancedclassify.dpclassify([25,40], avgs)
+advancedclassify.dpclassify([48,20], avgs)
+
+reload(advancedclassify)
+numericalset = advancedclassify.loadnumerical()
+numericalset[0].data
+
+reload(advancedclassify)
+scaledset, scalef = advancedclassify.scaledata(numericalset)
+avgs = advancedclassify.lineartrain(scaledset)
+numericalset[0].data
+
+reload(advancedclassify)
+offset = advancedclassify.getoffset(agesonly)
+advancedclassify.nlclassify([30,30], agesonly, offset)
+advancedclassify.nlclassify([48,32], agesonly, offset)
+advancedclassify.nlclassify([25,40], agesonly, offset)
+advancedclassify.nlclassify([48,20], agesonly, offset)
